a48ff8523ee347a91b4e505b59aa01852c94a10d
suni <sunielsharma1921@gmail.com>
2024-06-02 12:11:02 +0545

    navbar changed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,166 +1,36 @@
 
 
-
-// import { useState } from 'react';
-// import { Link, NavLink } from 'react-router-dom';
-// import { Menu, Dropdown } from 'antd';
-// import { DownOutlined } from '@ant-design/icons';
-// import '../navbar/navbar.css';
-// import Banner from '../Banner/Banner';
-// import { BiMenuAltRight, BiX } from "react-icons/bi";
-// // import { TbMenuDeep } from "react-icons/tb";
-
-
-// const Navbar = () => {
-
-//   const [showMenu, setShowMenu] = useState(false); // State to handle menu visibility
-
-//  // Function to toggle menu visibility
-//  const toggleMenu = () => {
-//   setShowMenu(!showMenu);
-// };
-
-
-//   const openLicensePdf = () => {
-//     window.open('/image/LINK-STAR-LICENSE.pdf');
-//   };
-
-//  const openCompanyProfilePdf = ()=>{
-//   window.open('/image/Link-Star-Brochure.pdf', '_blank')
-//  }
-
-  // const ourCompanyMenu = (
-  //   <Menu>
-  //     <Menu.Item key="1">
-  //       <NavLink to='/our-company/chairperson'>Words from Chairperson</NavLink>
-  //     </Menu.Item>
-  //     <Menu.Item key="2">
-  //       <NavLink to='/our-company/managing-director'>Words from Managing Director</NavLink>
-  //     </Menu.Item>
-  //     <Menu.Item key="3">
-  //       <NavLink to='/our-company/intl-marketing-director'>Words from Intl Marketing Director</NavLink>
-  //     </Menu.Item>
-  //     <Menu.Item key="4">
-  //       <NavLink to='/our-company/marketing-officer'>Words from Marketing Officer</NavLink>
-  //     </Menu.Item>
-  //     <Menu.Item key="5">
-  //       <NavLink to='/our-company/general-manager'>Words from General Manager</NavLink>
-  //     </Menu.Item>
-  //     <Menu.Item key="6">
-  //       <NavLink to='/our-company/BOD'>Words from BOD</NavLink>
-  //     </Menu.Item>
-  //     {/* <Menu.Item key="7">
-  //       <NavLink onClick={openLicensePdf}>View Company License</NavLink>
-  //     </Menu.Item> */}
-  //     <Menu.Item key="8">
-  //       <NavLink onClick={openCompanyProfilePdf}>View Company Profile</NavLink>
-  //     </Menu.Item>
-  //   </Menu>
-  // );
-
-  // const procedureMenu = (
-  //   <Menu>
-  //     <Menu.Item key="1">
-  //       <NavLink to='/procedure/why-nepalese'>Why Nepalese</NavLink>
-  //     </Menu.Item>
-  //     <Menu.Item key="2">
-  //       <NavLink to='/procedure/how-its-work'>How Its Work</NavLink>
-  //     </Menu.Item>
-  //     <Menu.Item key="3">
-  //       <NavLink to='/procedure/required-documents'>Required Documents</NavLink>
-  //     </Menu.Item>
-  //   </Menu>
-  // );
-
-//   return (
-//     <div className='navbarContainer container'>
-//       <div className='banner'><Banner /></div>
-//       <hr />
-//       <div className='navbar global_flex'>
-//         <img className='logo' src="/image/logopng.png" alt="iink star logo" />
-//         {/* <ul className='navlinks'> */}
-//         <ul className={`navlinks ${showMenu ? 'show' : ''}`}>
-//           <li><NavLink to='/'>Home</NavLink></li>
-//           <li>
-//             <Dropdown overlay={ourCompanyMenu}>
-//               <NavLink to='/our-company' className='ant-dropdown-link' onClick={e => e.preventDefault()}>
-//                 Our Company <DownOutlined />
-//               </NavLink>
-//             </Dropdown>
-//           </li>
-//           <li><NavLink to='/about'>About</NavLink></li>
-//           <li><NavLink to='/why-us'>Why Us?</NavLink></li>
-//           <li>
-//             <Dropdown overlay={procedureMenu}>
-//               <NavLink to='/procedure' className='ant-dropdown-link' onClick={e => e.preventDefault()}>
-//                 Procedure <DownOutlined />
-//               </NavLink>
-//             </Dropdown>
-//           </li>
-//           <li><NavLink to='/demands'>Demands</NavLink></li>
-       
-//           <li><Link to={'/'} onClick={openLicensePdf}>Legal Documents</Link></li>
-//           <li><NavLink to='/contact'>Contact Us</NavLink></li>
-//           <li><NavLink to='/apply-now'>Apply Now</NavLink></li>
-       
-//         </ul>
-//         {/* <div className='hambugerMenu'><TbMenuDeep/></div> */}
-//         {/* Hamburger Menu */}
-//         <p className='hambugerMenu' onClick={toggleMenu}>
-//             {showMenu ? <BiX /> : <BiMenuAltRight />}
-//           </p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Navbar;
+// *************dynamic code for demand page***************
 
 
 
-
-
-
-
-
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { Menu, Dropdown } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import '../navbar/navbar.css';
 import Banner from '../Banner/Banner';
 import { BiMenuAltRight, BiX } from "react-icons/bi";
+// import { TbMenuDeep } from "react-icons/tb";
+
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false);
-  const [navbarTop, setNavbarTop] = useState(53);
-  const [scrolled, setScrolled] = useState(false); // New state to track scroll
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrolled = window.scrollY > 0;
-      setScrolled(scrolled); // Update scroll state
-      setNavbarTop(scrolled ? -15 : 53); // Adjust navbar position based on scroll
-    };
+  const [showMenu, setShowMenu] = useState(false); // State to handle menu visibility
 
-    window.addEventListener('scroll', handleScroll);
+ // Function to toggle menu visibility
+ const toggleMenu = () => {
+  setShowMenu(!showMenu);
+};
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
 
   const openLicensePdf = () => {
     window.open('/image/LINK-STAR-LICENSE.pdf');
   };
 
-  const openCompanyProfilePdf = () => {
-    window.open('/image/Link-Star-Brochure.pdf', '_blank');
-  };
+ const openCompanyProfilePdf = ()=>{
+  window.open('/image/Link-Star-Brochure.pdf', '_blank')
+ }
 
   const ourCompanyMenu = (
     <Menu>
@@ -182,6 +52,9 @@ const Navbar = () => {
       <Menu.Item key="6">
         <NavLink to='/our-company/BOD'>Words from BOD</NavLink>
       </Menu.Item>
+      {/* <Menu.Item key="7">
+        <NavLink onClick={openLicensePdf}>View Company License</NavLink>
+      </Menu.Item> */}
       <Menu.Item key="8">
         <NavLink onClick={openCompanyProfilePdf}>View Company Profile</NavLink>
       </Menu.Item>
@@ -203,11 +76,12 @@ const Navbar = () => {
   );
 
   return (
-    <div className={`navbarContainer ${scrolled ? 'scrolled' : ''}`}>
-      <div className='banner container'><Banner /></div>
+    <div className='navbarContainer container'>
+      <div className='banner'><Banner /></div>
       <hr />
-      <div className='navbar global_flex' style={{ top: `${navbarTop}px` }}>
+      <div className='navbar global_flex'>
         <img className='logo' src="/image/logopng.png" alt="iink star logo" />
+        {/* <ul className='navlinks'> */}
         <ul className={`navlinks ${showMenu ? 'show' : ''}`}>
           <li><NavLink to='/'>Home</NavLink></li>
           <li>
@@ -219,7 +93,6 @@ const Navbar = () => {
           </li>
           <li><NavLink to='/about'>About</NavLink></li>
           <li><NavLink to='/why-us'>Why Us?</NavLink></li>
-          <li><NavLink to='/our-business-partner'>Our Clients & Business Partner</NavLink></li>
           <li>
             <Dropdown overlay={procedureMenu}>
               <NavLink to='/procedure' className='ant-dropdown-link' onClick={e => e.preventDefault()}>
@@ -228,16 +101,147 @@ const Navbar = () => {
             </Dropdown>
           </li>
           <li><NavLink to='/demands'>Demands</NavLink></li>
+       
           <li><Link to={'/'} onClick={openLicensePdf}>Legal Documents</Link></li>
           <li><NavLink to='/contact'>Contact Us</NavLink></li>
           <li><NavLink to='/apply-now'>Apply Now</NavLink></li>
+       
         </ul>
+        {/* <div className='hambugerMenu'><TbMenuDeep/></div> */}
+        {/* Hamburger Menu */}
         <p className='hambugerMenu' onClick={toggleMenu}>
-          {showMenu ? <BiX /> : <BiMenuAltRight />}
-        </p>
+            {showMenu ? <BiX /> : <BiMenuAltRight />}
+          </p>
       </div>
     </div>
   );
 }
 
 export default Navbar;
+
+
+
+
+
+// **************static code for demand page*************
+
+
+// import { useState, useEffect } from 'react';
+// import { Link, NavLink } from 'react-router-dom';
+// import { Menu, Dropdown } from 'antd';
+// import { DownOutlined } from '@ant-design/icons';
+// import '../navbar/navbar.css';
+// import Banner from '../Banner/Banner';
+// import { BiMenuAltRight, BiX } from "react-icons/bi";
+
+// const Navbar = () => {
+//   const [showMenu, setShowMenu] = useState(false);
+//   const [navbarTop, setNavbarTop] = useState(53);
+//   const [scrolled, setScrolled] = useState(false); // New state to track scroll
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const scrolled = window.scrollY > 0;
+//       setScrolled(scrolled); // Update scroll state
+//       setNavbarTop(scrolled ? -15 : 53); // Adjust navbar position based on scroll
+//     };
+
+//     window.addEventListener('scroll', handleScroll);
+
+//     return () => {
+//       window.removeEventListener('scroll', handleScroll);
+//     };
+//   }, []);
+
+//   const toggleMenu = () => {
+//     setShowMenu(!showMenu);
+//   };
+
+//   const openLicensePdf = () => {
+//     // window.open('/image/LINK-STAR-LICENSE.pdf');
+//     window.open("/image/linkStar_License 2024.pdf")
+//   };
+
+//   const openCompanyProfilePdf = () => {
+//     window.open('/image/Link-Star-Brochure.pdf', '_blank');
+//   };
+
+//   const ourCompanyMenu = (
+//     <Menu>
+//       <Menu.Item key="1">
+//         <NavLink to='/our-company/chairperson'>Words from Chairperson</NavLink>
+//       </Menu.Item>
+//       <Menu.Item key="2">
+//         <NavLink to='/our-company/managing-director'>Words from Managing Director</NavLink>
+//       </Menu.Item>
+//       <Menu.Item key="3">
+//         <NavLink to='/our-company/intl-marketing-director'>Words from Intl Marketing Director</NavLink>
+//       </Menu.Item>
+//       <Menu.Item key="4">
+//         <NavLink to='/our-company/marketing-officer'>Words from Marketing Officer</NavLink>
+//       </Menu.Item>
+//       <Menu.Item key="5">
+//         <NavLink to='/our-company/general-manager'>Words from General Manager</NavLink>
+//       </Menu.Item>
+//       <Menu.Item key="6">
+//         <NavLink to='/our-company/BOD'>Words from BOD</NavLink>
+//       </Menu.Item>
+//       <Menu.Item key="8">
+//         <NavLink onClick={openCompanyProfilePdf}>View Company Profile</NavLink>
+//       </Menu.Item>
+//     </Menu>
+//   );
+
+//   const procedureMenu = (
+//     <Menu>
+//       <Menu.Item key="1">
+//         <NavLink to='/procedure/why-nepalese'>Why Nepalese</NavLink>
+//       </Menu.Item>
+//       <Menu.Item key="2">
+//         <NavLink to='/procedure/how-its-work'>How Its Work</NavLink>
+//       </Menu.Item>
+//       <Menu.Item key="3">
+//         <NavLink to='/procedure/required-documents'>Required Documents</NavLink>
+//       </Menu.Item>
+//     </Menu>
+//   );
+
+//   return (
+//     <div className={`navbarContainer ${scrolled ? 'scrolled' : ''}`}>
+//       <div className='banner container'><Banner /></div>
+//       <hr />
+//       <div className='navbar global_flex' style={{ top: `${navbarTop}px` }}>
+//         <img className='logo' src="/image/logopng.png" alt="iink star logo" />
+//         <ul className={`navlinks ${showMenu ? 'show' : ''}`}>
+//           <li><NavLink to='/'>Home</NavLink></li>
+//           <li>
+//             <Dropdown overlay={ourCompanyMenu}>
+//               <NavLink to='/our-company' className='ant-dropdown-link' onClick={e => e.preventDefault()}>
+//                 Our Company <DownOutlined />
+//               </NavLink>
+//             </Dropdown>
+//           </li>
+//           <li><NavLink to='/about'>About</NavLink></li>
+//           <li><NavLink to='/why-us'>Why Us?</NavLink></li>
+//           <li><NavLink to='/our-business-partner'>Clients & Business Partner</NavLink></li>
+//           <li>
+//             <Dropdown overlay={procedureMenu}>
+//               <NavLink to='/procedure' className='ant-dropdown-link' onClick={e => e.preventDefault()}>
+//                 Procedure <DownOutlined />
+//               </NavLink>
+//             </Dropdown>
+//           </li>
+//           <li><NavLink to='/demands'>Demands</NavLink></li>
+//           <li><Link to={'/'} onClick={openLicensePdf}>Legal Documents</Link></li>
+//           <li><NavLink to='/contact'>Contact</NavLink></li>
+//           <li><NavLink to='/apply-now'>Apply Now</NavLink></li>
+//         </ul>
+//         <p className='hambugerMenu' onClick={toggleMenu}>
+//           {showMenu ? <BiX /> : <BiMenuAltRight />}
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
--- src/components/vacency/Vacency.jsx
+++ src/components/vacency/Vacency.jsx
@@ -1,40 +1,195 @@
-import { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
-import axios from 'axios';
-import '../vacency/vacency.css';
-import {Link} from 'react-router-dom'
+// import { useEffect, useState } from 'react';
+// import toast from 'react-hot-toast';
+// import axios from 'axios';
+// import '../vacency/vacency.css';
+// import {Link} from 'react-router-dom'
 
+
+// const Vacency = () => {
+//   const [vacancies, setVacancies] = useState([]);
+//   const [selectedImage, setSelectedImage] = useState(null);
+
+
+//   const getAllVacancy = async () => {
+//     try {
+//       const apiUrl = import.meta.env.VITE_API_URL;
+//       if (!apiUrl) {
+//         toast.error('API URL is not defined');
+//         return;
+//       }
+
+//       const response = await axios.get(`${apiUrl}/api/v1/upload/getAllVacency`);
+//       // console.log('API response:', response);
+
+//       if (response.data.success) {
+//         // console.log('Vacancies data:', response.data.data);
+//         setVacancies(response.data.data);
+//       } else {
+//         toast.error(response.data.message || 'No vacancies found');
+//       }
+//     } catch (error) {
+//       toast.error(error.response?.data?.message || 'An error occurred');
+//     }
+//   };
+
+//   useEffect(() => {
+//     getAllVacancy();
+//   }, []);
+
+//   const handleImageClick = (imageUrl) => {
+//     setSelectedImage(imageUrl);
+//   };
+
+//   const handleClosePopup = () => {
+//     setSelectedImage(null);
+//   };
+
+//   return (
+//     <div className='vacency_container'>
+//       <div className="container">
+//         <div className="vacency_content global_margin_top">
+//           <h1 style={headingStyle}>Current Open Vacancies</h1>
+//           {vacancies.length === 0 ? (
+//             <p>No vacancies found</p>
+//           ) : (
+//             <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+//               <thead>
+//                 <tr>
+//                   <th style={tableHeaderStyle}>Image</th>
+//                   <th style={tableHeaderStyle}>Position</th>
+//                   <th style={tableHeaderStyle}>Vacancy</th>
+//                   <th style={tableHeaderStyle}>Salary</th>
+//                   <th style={tableHeaderStyle}>Food</th>
+//                   <th style={tableHeaderStyle}>Accommodation</th>
+//                   <th style={tableHeaderStyle}>Transportation</th>
+//                   <th style={tableHeaderStyle}>Overtime</th>
+//                   <th style={tableHeaderStyle}>Apply</th>
+//                 </tr>
+//               </thead>
+//               <tbody>
+//                 {vacancies.map((vacancy) => (
+//                   <tr key={vacancy._id}>
+//                     <td style={tableCellStyle}>
+//                       <img 
+//                         src={vacancy.images[0]} 
+//                         alt={vacancy.position} 
+//                         width="100" 
+//                         style={{ cursor: 'pointer' }}
+//                         onClick={() => handleImageClick(vacancy.images[0])}
+//                       />
+//                     </td>
+//                     <td style={tableCellStyle}>{vacancy.position}</td>
+//                     <td style={tableCellStyle}>{vacancy.vacancy}</td>
+//                     <td style={tableCellStyle}>{vacancy.salary}</td>
+//                     <td style={tableCellStyle}>{vacancy.food}</td>
+//                     <td style={tableCellStyle}>{vacancy.accommodation}</td>
+//                     <td style={tableCellStyle}>{vacancy.transportation}</td>
+//                     <td style={tableCellStyle}>{vacancy.overtime}</td>
+//                     <td style={tableCellStyle}><Link to='/apply-now'>Apply Here</Link></td>
+//                   </tr>
+//                 ))}
+//               </tbody>
+//             </table>
+//           )}
+//         </div>
+//       </div>
+
+//       {selectedImage && (
+//         <div style={popupOverlayStyle}>
+//           <div style={popupContentStyle}>
+//             <span style={closeButtonStyle} onClick={handleClosePopup}>&times;</span>
+//             <img src={selectedImage} alt="Selected" style={{ width: '100%', maxHeight: '100%' }} />
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// const headingStyle = {
+//   textAlign: 'center',
+//   marginBottom: '20px',
+//   fontSize: '24px',
+//   color: '#333',
+// };
+
+// const tableHeaderStyle = {
+//   border: '1px solid black',
+//   padding: '8px',
+//   backgroundColor: '#f2f2f2'
+// };
+
+// const tableCellStyle = {
+//   border: '1px solid black',
+//   padding: '8px',
+//   textAlign: 'left',
+// };
+
+// const popupOverlayStyle = {
+//   position: 'fixed',
+//   top: 0,
+//   left: 0,
+//   width: '100%',
+//   height: '100%',
+//   backgroundColor: 'rgba(0, 0, 0, 0.7)',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center'
+// };
+
+// const popupContentStyle = {
+//   position: 'relative',
+//   background: '#fff',
+//   padding: '20px',
+//   borderRadius: '8px',
+//   maxWidth: '90%',
+//   maxHeight: '90%',
+//   overflow: 'auto'
+// };
+
+// const closeButtonStyle = {
+//   position: 'absolute',
+//   top: '10px',
+//   right: '10px',
+//   fontSize: '30px'
+// };
+
+// export default Vacency;
+
+
+// ****************static code*************
+
+import { useState } from 'react';
+import '../vacency/vacency.css';
+import { Link } from 'react-router-dom';
 
 const Vacency = () => {
-  const [vacancies, setVacancies] = useState([]);
+  const [vacancies] = useState([
+    {
+      _id: '1',
+      images: ['https://via.placeholder.com/100'],
+      position: 'Software Engineer',
+      vacancy: '5',
+      salary: '$5000',
+      food: 'Provided',
+      accommodation: 'Provided',
+      transportation: 'Provided',
+      overtime: 'Available',
+    },
+    {
+      _id: '2',
+      images: ['https://via.placeholder.com/100'],
+      position: 'Product Manager',
+      vacancy: '2',
+      salary: '$7000',
+      food: 'Provided',
+      accommodation: 'Provided',
+      transportation: 'Provided',
+      overtime: 'Available',
+    },
+    // Add more static vacancies as needed
+  ]);
   const [selectedImage, setSelectedImage] = useState(null);
-
-
-  const getAllVacancy = async () => {
-    try {
-      const apiUrl = import.meta.env.VITE_API_URL;
-      if (!apiUrl) {
-        toast.error('API URL is not defined');
-        return;
-      }
-
-      const response = await axios.get(`${apiUrl}/api/v1/upload/getAllVacency`);
-      // console.log('API response:', response);
-
-      if (response.data.success) {
-        // console.log('Vacancies data:', response.data.data);
-        setVacancies(response.data.data);
-      } else {
-        toast.error(response.data.message || 'No vacancies found');
-      }
-    } catch (error) {
-      toast.error(error.response?.data?.message || 'An error occurred');
-    }
-  };
-
-  useEffect(() => {
-    getAllVacancy();
-  }, []);
 
   const handleImageClick = (imageUrl) => {
     setSelectedImage(imageUrl);
@@ -116,7 +271,7 @@ const headingStyle = {
 const tableHeaderStyle = {
   border: '1px solid black',
   padding: '8px',
-  backgroundColor: '#f2f2f2'
+  backgroundColor: '#f2f2f2',
 };
 
 const tableCellStyle = {
@@ -134,7 +289,7 @@ const popupOverlayStyle = {
   backgroundColor: 'rgba(0, 0, 0, 0.7)',
   display: 'flex',
   justifyContent: 'center',
-  alignItems: 'center'
+  alignItems: 'center',
 };
 
 const popupContentStyle = {
@@ -144,14 +299,14 @@ const popupContentStyle = {
   borderRadius: '8px',
   maxWidth: '90%',
   maxHeight: '90%',
-  overflow: 'auto'
+  overflow: 'auto',
 };
 
 const closeButtonStyle = {
   position: 'absolute',
   top: '10px',
   right: '10px',
-  fontSize: '30px'
+  fontSize: '30px',
 };
 
 export default Vacency;
